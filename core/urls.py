a82e64594b104c3f56ec1cd428f82505610fb007
phanletrungtien50 <tienplt.50.student@fit.tdc.edu.vn>
2026-05-01 21:43:35 +0700

    add home route

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,20 @@
-# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse  # 👈 thêm
+
+def home(request):  # 👈 thêm
+    return JsonResponse({
+        "status": "ok",
+        "message": "Backend is running 🚀"
+    })
 
 urlpatterns = [
+    path('', home),  # 👈 thêm dòng này
     path('admin/', admin.site.urls),
-    path('api/', include('shop.urls')), # Mọi API sẽ bắt đầu bằng /api/
+    path('api/', include('shop.urls')),
 ]
 
-# Cấu hình để hiển thị ảnh khi chạy Local
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
